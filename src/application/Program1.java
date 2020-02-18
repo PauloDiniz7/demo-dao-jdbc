ea4174fba2f37621a2761216dd58e5db905f65ae
PauloDiniz7 <paulodiniz7@hotmail.com>
2020-02-18 09:08:43 -0300

    findByld implementation

--- src/application/Program1.java
+++ src/application/Program1.java
@@ -1,16 +1,17 @@
 package application;
 
-import java.util.Date;
+//import java.util.Date;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
+//import model.entities.Department;
 import model.entities.Seller;
 
 public class Program1 {
 	public static void main(String[] args) {
 		
-		Department obj = new Department (1, "Books");
+	// criando um departamento (1, Books) e um vendedor (id = 21, Bob,...)	
+	/*	Department obj = new Department (1, "Books");
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj );
 		
@@ -18,8 +19,13 @@ public class Program1 {
 						
 		System.out.println(obj);	
 		System.out.println("-------------------------");
-		System.out.println(seller);	
+		System.out.println(seller);	  */
 
-
+		//obter o vendedor de codigo (id) igual a 3
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 }
